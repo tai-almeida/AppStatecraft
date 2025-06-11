ed266f33474050bc4c0bf706ef5cfd07d489b17e
Aluno 45 <taina_bastosalmeida@hotmail.com>
2025-06-11 09:19:32 -0300

    create proj

---
+++ AppStatecraft/AppStatecraft/AppStatecraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStatecraftApp.swift
+//  AppStatecraft
+//
+//  Created by Aluno 45 on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AppStatecraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStatecraft/AppStatecraft/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppStatecraft
+//
+//  Created by Aluno 45 on 11/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

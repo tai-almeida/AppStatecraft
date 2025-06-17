b00961fb4091da6b22174b6528e3b57756c4076d
sofiavvb <s252974@dac.unicamp.br>
2025-06-17 09:13:36 -0300

    entidades das metodologias modeladas

---
+++ AppStatecraft/AppStatecraft/Core/Persistence/Classes/SessaoMaieutica+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  SessaoMaieutica+CoreDataClass.swift
+//  AppStatecraft
+//
+//  Created by Sofia Villas Bôas on 17/06/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(SessaoMaieutica)
+public class SessaoMaieutica: Sessao {
+
+}
---
+++ AppStatecraft/AppStatecraft/Core/Persistence/Properties/SessaoFreeWriting+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//
+//  SessaoFreeWriting+CoreDataProperties.swift
+//  AppStatecraft
+//
+//  Created by Sofia Villas Bôas on 17/06/25.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension SessaoFreeWriting {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<SessaoFreeWriting> {
+        return NSFetchRequest<SessaoFreeWriting>(entityName: "SessaoFreeWriting")
+    }
+
+    @NSManaged public var enunciado: String?
+
+}

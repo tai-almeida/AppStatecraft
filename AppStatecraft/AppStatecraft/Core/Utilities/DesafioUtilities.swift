f2c13cd6c4e86683903dbff9c4b6f3686179c086
Aluno 45 <taina_bastosalmeida@hotmail.com>
2025-06-23 09:34:35 -0300

    desafios utilities incompleto

--- AppStatecraft/AppStatecraft/Core/Utilities/DesafioUtilities.swift
+++ AppStatecraft/AppStatecraft/Core/Utilities/DesafioUtilities.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 
-class DesafioUtilities: ObservableObject {
+class DesafiosUtilities: ObservableObject {
     
     @Published var todosDesafios = [QuestaoDesafios]()
     @Published var desafiosFeitos = [QuestaoDesafios]()
@@ -16,7 +16,29 @@ class DesafioUtilities: ObservableObject {
 
     
     init() {
+        copiaJson()
         carregaDesafios()
+    }
+    
+    func copiaJson() {
+        // manipulacao de arquivos
+        let gerenciaArquivo = FileManager.default
+        
+        // obtem caminho ate o arquivo json com as questoes
+        let url = gerenciaArquivo.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let caminho = url.appendingPathComponent("BancoQuestoes.json")
+        
+        
+        // copia arquivo json e verifica se ha erros
+        if !gerenciaArquivo.fileExists(atPath: caminho.path){
+            if let origem = Bundle.main.url(forResource: "BancoQuestoes", withExtension: "json") {
+                do {
+                    try gerenciaArquivo.copyItem(at: origem, to: caminho)
+                } catch {
+                    print("erro ao copiar arquivo")
+                }
+            }
+        }
     }
     
     func carregaDesafios() {
@@ -42,8 +64,23 @@ class DesafioUtilities: ObservableObject {
         }catch {
             print("erro")
         }
-        
-        
-        
     }
+    
+    func atualizaJson() {
+        // atualiza o json apos manipular vetores com desafios completos e nao feitos pelo usuario
+        todosDesafios = desafiosFeitos + desafiosNaoFeitos
+        
+        do {
+            // codifica dados do vetor para o json e acessa o arquivo pelo caminho
+            let data = try JSONEncoder().encode(todosDesafios)
+            let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            let fileURL = documentsDirectory.appendingPathComponent("BancoQuestoes.json")
+            
+            // escreve no arquivo
+            try data.write(to: fileURL)
+        } catch {
+            print("Erro ao salvar JSON: \(error)")
+        }
+    }
+    
 }

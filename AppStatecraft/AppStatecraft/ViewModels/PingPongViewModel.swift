08606c851d1af39c3753b92180f8221f4e1ca1ff
anamargaridaborges <a252137@dac.unicamp.br>
2025-06-24 12:04:01 -0300

    iniciando lista de palavras

--- AppStatecraft/AppStatecraft/ViewModels/PingPongViewModel.swift
+++ AppStatecraft/AppStatecraft/ViewModels/PingPongViewModel.swift
@@ -6,10 +6,11 @@
 //
 
 import Foundation
+import UIKit
 
-class PingPongViewModel: ObservableObject {
+class PingPongViewModel: UIViewController, ObservableObject {
     
-    @Published var respostaIA: String
+    @Published var respostaIA: String = ""
     
     func fazerRequisicao(context: [String]) async -> String? {
         let url = URL(string: "https://api.replicate.com/v1/models/openai/gpt-4o-mini/predictions")!
@@ -101,7 +102,7 @@ class PingPongViewModel: ObservableObject {
         return nil
     }
     
-    init () {
-        self.respostaIA = ""
-    }
+//    init () {
+//        self.respostaIA = ""
+//    }
 }
--- AppStatecraft/AppStatecraft/Views/PingPong/PingPongView.swift
+++ AppStatecraft/AppStatecraft/Views/PingPong/PingPongView.swift
@@ -6,15 +6,26 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct PingPongView: View {
     @StateObject var viewModel = PingPongViewModel()
     @State var textoIA = ""
+    @State var palavras: [String] = []
+    @State var input = ""
     var body: some View {
-        Text(textoIA)
-            .task {
-                textoIA = await viewModel.fazerRequisicao(context: []) ?? "Erro"
+        VStack {
+            ForEach (palavras, id: \.self) { palavra in
+                Text(palavra)
             }
+            TextField ("Escreva aqui", text: $input)
+                .multilineTextAlignment(.center)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .task {
+            textoIA = await viewModel.fazerRequisicao(context: []) ?? "Erro"
+            palavras.append(textoIA)
+        }
     }
 }
 

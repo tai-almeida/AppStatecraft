58fb3250f2ea3fd16a34673f4bf666cda4ec60eb
Aluno 45 <taina_bastosalmeida@hotmail.com>
2025-06-25 12:00:47 -0300

    inicia logica cronometro

---
+++ AppStatecraft/AppStatecraft/Views/PingPong/TimerView.swift
@@ -0,0 +1,24 @@
+//
+//  TimerView.swift
+//  AppStatecraft
+//
+//  Created by Aluno 45 on 25/06/25.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    @Binding var minutes: Int
+    @Binding var seconds: Int
+//    var viewController = TimerViewController()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//struct TimerView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TimerView()
+//    }
+//}

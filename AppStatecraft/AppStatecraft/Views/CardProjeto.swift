7fc05ec0a445c8f3f9396f20529f7f56f40f018b
João Pedro Faria <jpfaria770@gmail.com>
2025-07-01 08:45:02 -0300

    feat: criando um Card para meus projetos

---
+++ AppStatecraft/AppStatecraft/Views/CardProjeto.swift
@@ -0,0 +1,20 @@
+//
+//  CardProjeto.swift
+//  AppStatecraft
+//
+//  Created by Aluno 24 on 30/06/25.
+//
+
+import SwiftUI
+
+struct CardProjeto: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardProjeto_Previews: PreviewProvider {
+    static var previews: some View {
+        CardProjeto()
+    }
+}

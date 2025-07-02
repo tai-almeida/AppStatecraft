d00f8c4026751dbfbbc91a01d9d44c1a0c6ce4f6
Aluno 45 <taina_bastosalmeida@hotmail.com>
2025-07-02 12:21:47 -0300

    criar novo projeto vazio - modal

--- AppStatecraft/AppStatecraft/Views/AddProjetoVazioView.swift
+++ AppStatecraft/AppStatecraft/Views/AddProjetoVazioView.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct AddProjetoVazioView: View {
+    @Binding var nomeProjeto: String
+    
     var body: some View {
-        Text("Modal")
+        VStack(alignment: .leading) {
+            Text("NOME DO PROJETO")
+                .foregroundColor(.secondary)
+            
+            Divider()
+            
+            TextField("Nome", text: $nomeProjeto)
+                .padding()
+                .frame(height: 40)
+                .background(.white)
+                .cornerRadius(8)
+            
+            Text("Capa")
+                .foregroundColor(.secondary)
+                .padding(.top)
+            Divider()
+                .padding(.bottom)
+                        
+            Button(action: {
+                
+            }) {
+                HStack {
+                    Text("Escolher fotos existentes")
+                    
+                    Spacer()
+                    
+                    Image(systemName: "chevron.right")
+                        .foregroundColor(Color(.tertiaryLabel))
+                        .background(.white)
+
+
+                }
+            }
+                
+                
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
+        .background(Color(UIColor.secondarySystemBackground))
+
+        
     }
 }
 
-struct AddProjetoVazioView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddProjetoVazioView()
-    }
-}
+//struct AddProjetoVazioView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AddProjetoVazioView()
+//    }
+//}

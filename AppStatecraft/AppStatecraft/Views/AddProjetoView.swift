e55beff5ae2105d6b7c2d93ee1a2203b3b3a373f
sofiavvb <s252974@dac.unicamp.br>
2025-07-03 09:58:07 -0300

    teste de fetch projetos no modal

--- AppStatecraft/AppStatecraft/Views/AddProjetoView.swift
+++ AppStatecraft/AppStatecraft/Views/AddProjetoView.swift
@@ -11,8 +11,7 @@ import CoreData
 struct AddProjetoView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(\.managedObjectContext) private var contexto
-    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Projeto.data, ascending: false)])
-    private var projetos: FetchedResults<Projeto>
+    @State private var projetos: [Projeto] = []
     private let columns = [
         GridItem(.adaptive(minimum: 80))
     ]
@@ -20,30 +19,51 @@ struct AddProjetoView: View {
     @State var sessao: Sessao?
     @State private var telaCriarNovoProjeto = false
     
-    
     var body: some View {
-        
         NavigationView {
             ScrollView {
                 VStack {
                     LazyVGrid(columns: columns, spacing: 20) {
                         ForEach(projetos, id: \.self) { projeto in
-                            
+                            Button(action:{
+                                //aqui eh para salvar o contexto
+                                dismiss()
+                                self.metodologiaAparecendo = false
+                                Text(projeto.nome ?? "vazio")
+                            }){
+                                Text("nao tem projeto por enquanto amou")
+                            }
                         }
                     }
+                }.sheet(isPresented: $telaCriarNovoProjeto) {
+                    //criarEAddProjetoView(isPresented: $telaCriarNovoProjeto)
                 }
             }
             .navigationBarTitle(Text("Meus Projetos"))
             .navigationBarItems(
                 leading: Button("Cancelar") {
                     dismiss()
-                    self.metodologiaAparecendo = false
                 },
                 trailing: Button(action: {
                     self.telaCriarNovoProjeto = true
                 }) {
                     Image(systemName: "plus")
                 }).foregroundColor(.accentColor)
-            }
+        }.onAppear {
+            getAllProjetos(contexto: contexto)
+        }
+    }
+    
+    private func getAllProjetos(contexto: NSManagedObjectContext){
+        let requisicao = NSFetchRequest<Projeto>(entityName: "Projeto")
+        let ordenadorDeData = NSSortDescriptor(keyPath: \Projeto.data, ascending: false) //mais recente para o mais velho
+        requisicao.sortDescriptors = [ordenadorDeData]
+        
+        do {
+            projetos = try contexto.fetch(requisicao)
+            print("Busca concluída: \(projetos.count) projetos encontradas.")
+        } catch let error {
+            print("Erro: \(error.localizedDescription)")
+        }
     }
 }
--- AppStatecraft/AppStatecraft/Views/CriarEAddProjetoView.swift
+++ AppStatecraft/AppStatecraft/Views/CriarEAddProjetoView.swift
@@ -1,124 +1,124 @@
+////
+////  CriarEAddProjetoView.swift
+////  AppStatecraft
+////
+////  Created by Aluno 07 on 30/06/25.
+////
 //
-//  CriarEAddProjetoView.swift
-//  AppStatecraft
+//import Foundation
+//import SwiftUI
+//import CoreData
 //
-//  Created by Aluno 07 on 30/06/25.
-//
-
-import Foundation
-import SwiftUI
-import CoreData
-
-struct criarEAddProjetoView: View {
-    @Binding var isPresented: Bool
-    
-    @Environment(\.managedObjectContext) private var viewContext
-    @Environment(\.dismiss) private var dismiss
-    
-    let respostaTexto: String?
-    let respostaFoto: UIImage? //ver depois de passar como objeto mesmo para ficar mais legivel
-    let desafio: QuestaoDesafios
-    
-    @State private var nomeNovoProj = ""
-    
-    var body: some View {
-        NavigationView {
-            VStack {
-                Form{
-                    Section(header: Text("Nome do Projeto")) {
-                        TextField("ex: projeto de design", text: $nomeNovoProj)
-                    }
-                }
-                   .navigationBarTitle("Novo Projeto")
-                   .navigationBarTitleDisplayMode(.inline)
-                    .toolbar {
-                        ToolbarItem(placement: .cancellationAction) {
-                            Button("Cancelar") {
-                                dismiss()
-                            }
-                            foregroundColor(.accentColor)
-                        }
-                        ToolbarItem(placement: .confirmationAction) {
-                            Button("Salvar") {
-                                salvarSessao()
-                                print("salvou?")
-                                dismiss()
-                            }
-                            foregroundColor(.accentColor)
-                        }
-                    }
-            }
-        }
-    }
-    
-    private func salvarSessao() {
-        print("FUNCAO CHAMADA")
-        print("--- Dentro de salvarSessao. Tipo do desafio: '\(desafio.tipo)' ---")
-        
-        let novoProjeto = Projeto(context: viewContext)
-        novoProjeto.id = UUID()
-        novoProjeto.data = Date()
-        novoProjeto.nome = nomeNovoProj
-        novoProjeto.finalizado = false
-        novoProjeto.imagemCapa = Data()
-        
-        switch desafio.tipo {
-        case "maieutica":
-            let novaSessao = SessaoMaieutica(context: viewContext)
-            novaSessao.id = UUID()
-            novaSessao.data = Date()
-            
-            novaSessao.log = Data()
-            
-            novaSessao.projeto = novoProjeto
-        
-        case "texto":
-            let novaSessao = SessaoDesafioMult(context: viewContext)
-            novaSessao.id = UUID()
-            novaSessao.data = Date()
-            
-            novaSessao.desafioFeito = true
-            novaSessao.desafioID = Int64(self.desafio.id)
-            novaSessao.enunciado = self.desafio.enunciado
-          //  novaSessao.mediaFoto = Data()
-           // novaSessao.mediaTexto =
-          //  novaSessao.respostaFoto =
-            novaSessao.respostaTexto = self.respostaTexto
-            
-            novaSessao.projeto = novoProjeto
-            
-        case "free writing":
-            let novaSessao = SessaoFreeWriting(context: viewContext)
-            novaSessao.id = UUID()
-            novaSessao.data = Date()
-            
-            novaSessao.enunciado = String()
-            novaSessao.resposta = String()
-            
-            novaSessao.projeto = novoProjeto
-            
-        case "ping pong":
-            let novaSessao = SessaoPingPong(context: viewContext)
-            novaSessao.id = UUID()
-            novaSessao.data = Date()
-            
-            novaSessao.log = Data()
-            
-            novaSessao.projeto = novoProjeto
-        
-        default:
-            print("tipo invalido")
-            return
-            
-        }
-        
-        do {
-            try viewContext.save()
-            print("eeeeeee :)")
-            dismiss()
-        } catch {
-            let nsError = error as NSError
-            print("aaaah :( \(nsError)")
-        }
-    }
-}
+//struct criarEAddProjetoView: View {
+//    @Binding var isPresented: Bool
+//    
+//    @Environment(\.managedObjectContext) private var viewContext
+//    @Environment(\.dismiss) private var dismiss
+//    
+//    //let respostaTexto: String?
+//    //let respostaFoto: UIImage? //ver depois de passar como objeto mesmo para ficar mais legivel
+//    //let desafio: QuestaoDesafios
+//    
+//    @State private var nomeNovoProj = ""
+//    
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                Form{
+//                    Section(header: Text("Nome do Projeto")) {
+//                        TextField("ex: projeto de design", text: $nomeNovoProj)
+//                    }
+//                }
+//                   .navigationBarTitle("Novo Projeto")
+//                   .navigationBarTitleDisplayMode(.inline)
+//                    .toolbar {
+//                        ToolbarItem(placement: .cancellationAction) {
+//                            Button("Cancelar") {
+//                                dismiss()
+//                            }
+//                            foregroundColor(.accentColor)
+//                        }
+//                        ToolbarItem(placement: .confirmationAction) {
+//                            Button("Salvar") {
+//                                salvarSessao()
+//                                print("salvou?")
+//                                dismiss()
+//                            }
+//                            foregroundColor(.accentColor)
+//                        }
+//                    }
+//            }
+//        }
+//    }
+//    
+//    private func salvarSessao() {
+//        print("FUNCAO CHAMADA")
+//        print("--- Dentro de salvarSessao. Tipo do desafio: '\(desafio.tipo)' ---")
+//        
+//        let novoProjeto = Projeto(context: viewContext)
+//        novoProjeto.id = UUID()
+//        novoProjeto.data = Date()
+//        novoProjeto.nome = nomeNovoProj
+//        novoProjeto.finalizado = false
+//        novoProjeto.imagemCapa = Data()
+//        
+//        switch desafio.tipo {
+//        case "maieutica":
+//            let novaSessao = SessaoMaieutica(context: viewContext)
+//            novaSessao.id = UUID()
+//            novaSessao.data = Date()
+//            
+//            novaSessao.log = Data()
+//            
+//            novaSessao.projeto = novoProjeto
+//        
+//        case "texto":
+//            let novaSessao = SessaoDesafioMult(context: viewContext)
+//            novaSessao.id = UUID()
+//            novaSessao.data = Date()
+//            
+//            novaSessao.desafioFeito = true
+//            novaSessao.desafioID = Int64(self.desafio.id)
+//            novaSessao.enunciado = self.desafio.enunciado
+//          //  novaSessao.mediaFoto = Data()
+//           // novaSessao.mediaTexto =
+//          //  novaSessao.respostaFoto =
+//            novaSessao.respostaTexto = self.respostaTexto
+//            
+//            novaSessao.projeto = novoProjeto
+//            
+//        case "free writing":
+//            let novaSessao = SessaoFreeWriting(context: viewContext)
+//            novaSessao.id = UUID()
+//            novaSessao.data = Date()
+//            
+//            novaSessao.enunciado = String()
+//            novaSessao.resposta = String()
+//            
+//            novaSessao.projeto = novoProjeto
+//            
+//        case "ping pong":
+//            let novaSessao = SessaoPingPong(context: viewContext)
+//            novaSessao.id = UUID()
+//            novaSessao.data = Date()
+//            
+//            novaSessao.log = Data()
+//            
+//            novaSessao.projeto = novoProjeto
+//        
+//        default:
+//            print("tipo invalido")
+//            return
+//            
+//        }
+//        
+//        do {
+//            try viewContext.save()
+//            print("eeeeeee :)")
+//            dismiss()
+//        } catch {
+//            let nsError = error as NSError
+//            print("aaaah :( \(nsError)")
+//        }
+//    }
+//}

1d457d8d4560ae879996857f733f3c4221c84ecf
sofiavvb <s252974@dac.unicamp.br>
2025-07-04 09:53:37 -0300

    add picker no modal

--- AppStatecraft/AppStatecraft/Views/AddProjetoView.swift
+++ AppStatecraft/AppStatecraft/Views/AddProjetoView.swift
@@ -16,28 +16,58 @@ struct AddProjetoView: View {
     @Binding var metodologiaAparecendo: Bool
     @State var sessao: Sessao?
     @State private var telaCriarNovoProjeto = false
+    //@Binding var pesquisarProjeto: String
+    @State private var projetosConcluidos = "Em andamento"
     
+    private var projetosFiltrados: [Projeto] {
+            if projetosConcluidos == "Em andamento" {
+                return projetosVM.projetos.filter { !$0.finalizado }
+            } else {
+                return projetosVM.projetos.filter { $0.finalizado }
+            }
+        }
+
     var body: some View {
         NavigationView {
-            VStack {
-                ScrollView{
-                    LazyVGrid(columns: colunaCard, spacing: 20) {
-                        ForEach(projetosVM.projetos, id: \.self) { projeto in
-                            Button(action:{
-                                if let sessao = sessao{
-                                    projeto.addToSessoes(sessao)
-                                    projetosVM.salvar(contexto: contexto)
+            VStack() {
+                
+                Picker("", selection: $projetosConcluidos) {
+                    Text("Em andamento").tag("Em andamento")
+                    Text("Concluido").tag("Concluido")
+                }
+                .pickerStyle(.segmented)
+                .padding(.horizontal)
+                
+                VStack(alignment: .center){
+                    ScrollView{
+                        
+                        if projetosFiltrados.isEmpty{
+                            Text("Não há projetos aqui.")
+                        }
+                        
+                        LazyVGrid(columns: colunaCard, spacing: 20) {
+                            ForEach(projetosFiltrados, id: \.self) { projeto in
+                                Button(action:{
+                                    if let sessao = sessao{
+                                        projeto.addToSessoes(sessao)
+                                        projetosVM.salvar(contexto: contexto)
+                                    }
+                                    dismiss()
+                                    self.metodologiaAparecendo = false
+                                }){
+                                    projetoCardView(projeto: projeto)
                                 }
-                                dismiss()
-                                self.metodologiaAparecendo = false
-                            }){
-                                projetoCardView(projeto: projeto)
                             }
-                        }.padding()
+                        }
                     }.sheet(isPresented: $telaCriarNovoProjeto) {
                         //criarEAddProjetoView(isPresented: $telaCriarNovoProjeto)
                     }
                 }
+                //.frame(maxWidth: .infinity) nao sei o quanto isso realmente eh necessario
+                .padding(.horizontal)
+                
+                
+                
             }
             .navigationBarTitle(Text("Meus Projetos"))
             .navigationBarItems(

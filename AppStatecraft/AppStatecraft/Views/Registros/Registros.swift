b6c9b8ed9bbdf2afd68f783d96a8837d9d4b8baf
sofiavvb <s252974@dac.unicamp.br>
2025-08-12 08:43:17 -0300

    inicio dos registros

--- AppStatecraft/AppStatecraft/Views/Registros/Registros.swift
+++ AppStatecraft/AppStatecraft/Views/Registros/Registros.swift
@@ -2,17 +2,31 @@
 //  Registros.swift
 //  AppStatecraft
 //
-//  Created by Sofia Villas Bôas on 12/08/25.
+//  Created by Sofia Villas Bôas on 11/08/25.
 //
 
 import SwiftUI
 
 struct Registros: View {
+    @FetchRequest(
+        sortDescriptors:
+            [NSSortDescriptor(keyPath: \Registro.data, ascending: false)]
+    ) var registros: FetchedResults<Registro>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack(){
+                ForEach(registros, id: \.self) { registro in
+                    //card do registro ver que componente usar
+                }
+            }
+            .navigationTitle(Text("Registros"))
+            .navigationBarItems(trailing: Button(action: {
+                //adicionar registro
+            }){
+                Image(systemName: "plus")
+            })
+        }
     }
 }
 
-#Preview {
-    Registros()
-}
--- AppStatecraft/AppStatecraft/Views/TabBar.swift
+++ AppStatecraft/AppStatecraft/Views/TabBar.swift
@@ -1,25 +1,28 @@
 import SwiftUI
 
-
 struct TabBar: View {
     var body: some View {
-        
         TabView {
             MetodologiasView()
                 .tabItem {
                     Label("Metodologias", systemImage: "book")
                 }
-            
+            Registros()
+                .tabItem {
+                    Label("Registros", systemImage: "note.text")
+                }
             ProjetosView()
                 .tabItem {
                     Label("Projetos", systemImage: "folder")
-            }
+                }
         }
     }
 }
-
 //struct TabBar_Previews: PreviewProvider {
-//    static var previews: some View {
-//        TabBar()
-//    }
+//  static var previews: some View {
+//    TabBar()
+//  }
 //}
+
+
+

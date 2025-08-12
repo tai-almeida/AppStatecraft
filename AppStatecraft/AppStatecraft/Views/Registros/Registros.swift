bf05c97229661cd2a09ecda64ead58d98a6fafb9
sofiavvb <s252974@dac.unicamp.br>
2025-08-12 08:39:47 -0300

    Stop tracking Secrets.plist

---
+++ AppStatecraft/AppStatecraft/Views/Registros/Registros.swift
@@ -0,0 +1,18 @@
+//
+//  Registros.swift
+//  AppStatecraft
+//
+//  Created by Sofia Villas Bôas on 12/08/25.
+//
+
+import SwiftUI
+
+struct Registros: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Registros()
+}
